e68052190c6a97e1172f2213e148e3da6f27ac92
Andre LFA <andrelfaareas@gmail.com>
2020-05-28 23:08:26 -0300

    Entendendo rotas e parâmetros

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+// Tipos de Parâmetros:
+
+// Query params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação) // request.query 
+// Route params: Paramêtros não nomeado utilizados para identificar recursos // request.params
+// Request Body: Corpo da requisição, utilizado para alterar ou criar recursos
+
+app.post('/users', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
     return response.json({
         evento: 'Semana Omnistack 11',
         aluno: 'André LFA'
