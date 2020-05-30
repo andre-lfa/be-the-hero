1b68934f14290b373d29067774e7cf59a3bdc527
Andre LFA <andrelfaareas@gmail.com>
2020-05-30 12:19:56 -0300

    Cria o controller de incidents com os métodos create e index

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const org_controller = require('./controllers/OngController');
+const incident_controller = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
+// Rotas para Ongs
 routes.get('/ongs', org_controller.index);
-
 routes.post('/ongs', org_controller.create);
+
+// Rotas para Incidents
+routes.post('/incidents', incident_controller.create);
+routes.get('/incidents', incident_controller.index);
 
 module.exports = routes;
