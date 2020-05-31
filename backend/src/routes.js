abae6649fb8be4edc8a24564e585cca1af3bada0
Andre LFA <andrelfaareas@gmail.com>
2020-05-30 23:10:40 -0300

    Cria a rota e a lógica para deletar incidentes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,5 +11,6 @@ routes.post('/ongs', org_controller.create);
 // Rotas para Incidents
 routes.post('/incidents', incident_controller.create);
 routes.get('/incidents', incident_controller.index);
+routes.delete('/incidents/:id', incident_controller.delete);
 
 module.exports = routes;
