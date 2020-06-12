cdba8d51ab71e83bd7070fc99fc960260c382a1a
Andre LFA <andrelfaareas@gmail.com>
2020-06-12 01:56:34 -0300

    Faz a comunicação com a API para listar os incidentes

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 import './styles.css';
+import api from '../../services/api';
 
 function Profile () {
+    const [incidents, setIncidents] = useState([]);
+    const ongName = localStorage.getItem('ongName');
+    const ongId = localStorage.getItem('ongID');
+
+    useEffect(() => {
+        api.get('profile', {
+            headers: {
+                Authorization: ongId
+            }
+        }).then(response => {
+            setIncidents(response.data);
+        })
+    }, [ongId]);
+
     return(
         <div className="profile-container">
             <header>
                 <img src={logo} alt="Be the hero!"/>
-                <span>Bem vinda, APAD</span>
+                <span>Bem vinda, { ongName }</span>
                 <Link className="button" to="/incident/new">Cadastrar novo caso</Link>
                 <button type="button">
                     <FiPower size={18} color="#E02041"/>
@@ -19,50 +34,19 @@ function Profile () {
             <h1>Casos cadastrados</h1>
 
             <ul>
-                <li>
+                {incidents.map(incident => (
+                    <li key={incident.id}>
                     <strong>CASO</strong>
-                    <p>Título do caso</p>
+                    <p>{incident.title}</p>
                     <strong>DESCRIÇÃO</strong>
-                    <p>Descrição do caso</p>
+                    <p>{incident.description}</p>
                     <strong>VALOR</strong>
-                    <p>Valor que precisa arrecadar</p>
+                    <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
                     <button type="button">
                         <FiTrash2 size={20} color="#a8a8b3"/>
                     </button>
                 </li>
-                <li>
-                    <strong>CASO</strong>
-                    <p>Título do caso</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descrição do caso</p>
-                    <strong>VALOR</strong>
-                    <p>Valor que precisa arrecadar</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO</strong>
-                    <p>Título do caso</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descrição do caso</p>
-                    <strong>VALOR</strong>
-                    <p>Valor que precisa arrecadar</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
-                <li>
-                    <strong>CASO</strong>
-                    <p>Título do caso</p>
-                    <strong>DESCRIÇÃO</strong>
-                    <p>Descrição do caso</p>
-                    <strong>VALOR</strong>
-                    <p>Valor que precisa arrecadar</p>
-                    <button type="button">
-                        <FiTrash2 size={20} color="#a8a8b3"/>
-                    </button>
-                </li>
+                ))}
             </ul>
         </div>
     );
