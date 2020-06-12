cfbf6e90c808c48b28c5bbcf09676041cc92f900
Andre LFA <andrelfaareas@gmail.com>
2020-06-12 01:30:50 -0300

    Faz a conexão com a API para efetuar login na aplicação

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 import { FiLogIn } from 'react-icons/fi';
+import api from '../../services/api';
 
 
 function Logon() {
+    const history = useHistory();
+    const [id, setId] = useState('');
+
+    async function handleLogin(event){
+        event.preventDefault();
+
+        try {
+            const response = await api.post('sessions', { id });
+            console.log(response.data.name);
+
+            localStorage.setItem('ongID', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        } catch (error) {
+            alert("Não foi possível efetuar login. Tente novamente.");
+        }
+    }
+
     return (
         <div className="logon-container">
-            <section className="form">
+            <section className="form" onSubmit={handleLogin}>
                 <img src={logoImg} alt="Be the hero"/>
                 <form>
                     <h1>Faça seu logon</h1>
-                    <input placeholder="Sua ID"/>
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className= "button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
